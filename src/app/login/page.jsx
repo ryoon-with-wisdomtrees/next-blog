d07ea6bd552094f6018cc20138c4030d82f191e8
devseryoon <sphere.sryn@gmail.com>
2023-09-21 02:24:34 +0900

    [에러] cannot update a component ('...') while rendering a different 에러 수정

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,19 +2,22 @@
 import { signIn, useSession } from "next-auth/react";
 import styles from "./loginPage.module.css";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const LoginPage = () => {
   const { status } = useSession();
 
   const router = useRouter();
 
-  if (status === "loading") {
-    return <div className={styles.loading}>Loading...</div>;
-  }
+  useEffect(() => {
+    // if (status === "loading") {
+    //   return <div className={styles.loading}>Loading...</div>;
+    // }
 
-  if (status === "authenticated") {
-    router.push("/");
-  }
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [status]);
 
   return (
     <div className={styles.container}>
